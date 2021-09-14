8ebc32620e2fab58be7aafc9c141790760e68902
Aquila <aquila.choi10@gmail.com>
2021-09-14 17:45:49 -0400

    fixed comments

--- aspirationsproject/src/main/java/com/aspirations/project/aspirationsproject/base.java
+++ aspirationsproject/src/main/java/com/aspirations/project/aspirationsproject/base.java
@@ -2,13 +2,11 @@ package com.aspirations.project.aspirationsproject;
 
 import java.util.Iterator;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 
 public class base {
 public WebDriver driver;
@@ -71,24 +69,3 @@ public WebDriver driver;
 	}
 }
 }
-	/*
-	public void getNthResult(int productNumber) {
-		String xpathExpression = String.format("//div[@id='srp-river-results']/ul/li[%d]", productNumber);
-		String nthProduct = driver.findElement(By.xpath(xpathExpression)).getText();
-		System.out.println(nthProduct);
-	}
-	*/
-/*	public void getAllProducts() {
-		List<WebElement> allProducts = driver.findElements(By.xpath("//div[@id='srp-river-results']/ul/li/div/div[2]/a/h3"));
-		List<WebElement> allProducts2 = driver.findElements(By.xpath("//div[@id='srp-river-results']/ul/li/div/div[2]/div[3]/div[1]/span"));
-		Iterator<WebElement> iteratorAllProducts = allProducts.iterator();
-		Iterator<WebElement> iteratorAllProducts2 = allProducts2.iterator();
-		while(iteratorAllProducts.hasNext() && iteratorAllProducts2.hasNext()) {
-			WebElement product = iteratorAllProducts.next();
-			WebElement product2 = iteratorAllProducts2.next();
-			System.out.println(product.getText() + " - " + product2.getText());
-			System.out.println("-----------------------");
-		}
-	}
-	*/
-
